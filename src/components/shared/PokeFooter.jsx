f73feeda181453877ad80f17dd733a906830c14b
eliprival <eliprival@gmail.com>
2024-07-05 11:51:45 -0500

    added a footer

--- src/components/shared/PokeFooter.jsx
+++ src/components/shared/PokeFooter.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import "./styles/pokeFooter.css";
 
 const PokeFooter = () => {
-  return <div>PokeFooter</div>;
+  return (
+    <div className="pokefooter">
+      <div className="pokefooter__black">
+        <div className="pokefooter__outercircle">
+          <div className="pokefooter__innercircle"></div>
+        </div>
+      </div>
+      <div className="pokefooter__red">
+        © 2024 Pokédex Inc. | All Rights Reserved
+      </div>
+    </div>
+  );
 };
 
 export default PokeFooter;
